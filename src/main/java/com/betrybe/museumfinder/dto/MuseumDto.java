d5b202ca5ba58fc49a69ceb913c6ac249e8f424a
gabriel <soutogabriel04@gmail.com>
2024-01-29 16:20:01 -0300

    feat: model and dtos created

---
+++ src/main/java/com/betrybe/museumfinder/dto/MuseumDto.java
@@ -0,0 +1,21 @@
+package com.betrybe.museumfinder.dto;
+
+import com.betrybe.museumfinder.model.Coordinate;
+
+/**
+ * Museum dto class.
+ */
+
+public record MuseumDto(
+    Long id,
+    String name,
+    String description,
+    String address,
+    String collectionType,
+    String subject,
+    String url,
+    Coordinate coordinate
+) {
+
+}
+
